4d2b2d8af2faf055660ed88fe6fefa45e2bfeee7
Samidhisanjana <sanjanasilva1@gmail.com>
2019-02-21 15:34:26 +0530

    file added

---
+++ home.php
@@ -0,0 +1,35 @@
+<DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+<header>
+	<div class="row">
+		<div class="logo">
+			<img src="">
+		</div>
+		<ul class="main-nav">
+			<li><a href="" style="border: 1px solid black;">HOME</a></li>
+			<li><a href="">CONTACT</a></li>
+			<li><a href="">ABOUT US</a></li>
+
+
+		</ul>
+	</div>
+	<center>
+	<div class="hero">
+	<h1> HSHJSJHS HSSAKKJSK</h1>
+	<div class="button" style="text-align: center;">
+		<a href="" class="btn">GIRLS</a>
+		<a href="" class="btn"> BOYS</a>
+	</div>
+
+</div></center>
+
+</header>
+
+
+</body>
+</html>
